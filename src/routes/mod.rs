54a9a9a08efda264ff0830d24cdc815b8e746283
huyHA9597 <haanhuy5@gmail.com>
2023-04-17 07:25:18 +0700

    Add post method

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,5 +1,15 @@
-use axum::{routing::get, Router};
+mod hello_world;
+mod mirror_body_string;
+
+use axum::{
+    routing::{delete, get, patch, post, put},
+    Router,
+};
+use hello_world::hello_world;
+use mirror_body_string::mirror_body_string;
 
 pub fn create_route() -> Router {
-    Router::new().route("/", get(|| async { "Hello World!!!" }))
+    Router::new()
+        .route("/hello_world", get(hello_world))
+        .route("/mirror_body_string", post(mirror_body_string))
 }
